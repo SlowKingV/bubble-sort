42da4af5613f34dd57a85c71be470d75b0bb3e99
Santiago Rueda Ortiz <srueda27@gmail.com>
2020-01-28 11:17:10 -0500

    Add bubble_sort_by method
    It receives a block to compared the elements, similar to the spaceship operator

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,18 +1,40 @@
 def bubble_sort(arr)
-    array_length = arr.length
-    while array_length > 1
-        newn = 0
-        1.upto(array_length - 1) do |i|
-            if arr[i - 1] > arr[i]
-                temp = arr[i - 1]
-                arr[i - 1] = arr[i]
-                arr[i] = temp
-                newn = i
-            end
-        end
-        array_length = newn
+  array_length = arr.length
+  while array_length > 1
+    newn = 0
+    1.upto(array_length - 1) do |i|
+      next unless arr[i - 1] > arr[i]
+
+      temp = arr[i - 1]
+      arr[i - 1] = arr[i]
+      arr[i] = temp
+      newn = i
     end
-    arr
+    array_length = newn
+  end
+  arr
 end
 
-p bubble_sort([4,3,78,2,0,2])
+p bubble_sort([4, 3, 78, 2, 0, 2])
+
+def bubble_sort_by(arr)
+  array_length = arr.length
+
+  while array_length > 1
+    newn = 0
+    1.upto(array_length - 1) do |i|
+      next unless (yield arr[i - 1], arr[i]).positive?
+
+      temp = arr[i - 1]
+      arr[i - 1] = arr[i]
+      arr[i] = temp
+      newn = i
+    end
+    array_length = newn
+  end
+  arr
+end
+
+p bubble_sort_by(%w[hi hello hey]) do |left, right|
+  left.length - right.length
+end
