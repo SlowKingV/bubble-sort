d1b05fe79edfe8316d53c98d7de3266edffbc021
Diego Luna <slowkingvii@gmail.com>
2020-01-28 10:02:07 -0600

    Feature: Add Bubble sort method

---
+++ bubble_sort.rb
@@ -0,0 +1,18 @@
+def bubble_sort(arr)
+    array_length = arr.length
+    while array_length > 1
+        newn = 0
+        1.upto(array_length - 1) do |i|
+            if arr[i - 1] > arr[i]
+                temp = arr[i - 1]
+                arr[i - 1] = arr[i]
+                arr[i] = temp
+                newn = i
+            end
+        end
+        array_length = newn
+    end
+    arr
+end
+
+p bubble_sort([4,3,78,2,0,2])
