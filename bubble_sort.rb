48026de266642c06e9ba710a1023a610ca5d07e0
Santiago Rueda Ortiz <srueda27@gmail.com>
2020-01-28 14:14:14 -0500

    change the test for the bubble_sort_by

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -35,6 +35,4 @@ def bubble_sort_by(arr)
   arr
 end
 
-p bubble_sort_by(%w[hi hello hey]) do |left, right|
-  left.length - right.length
-end
+p bubble_sort_by(%w[hi hello hey]) { |left, right| left.length - right.length }
